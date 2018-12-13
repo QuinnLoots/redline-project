b9905ffb68ca763e9062a6be7fa7a06632db2859
Quinn Loots <loots.quinn@gmail.com>
2018-12-13 14:52:23 +0100

    test swipeable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,8 @@
-$(document).ready(function() {  
-
-					//Enable swiping...
-					$(".carousel-inner").swipe( {
-						//Generic swipe handler for all directions
-						swipeLeft:function(event, direction, distance, duration, fingerCount) {
-							$(this).parent().carousel('prev'); 
-						},
-						swipeRight: function() {
-							$(this).parent().carousel('next'); 
-						},
-						//Default is 75px, set to 0 for demo so any distance triggers swipe
-						threshold:0
-					});
-				});
+$(document).ready(function() {
+  $("#carouselExampleControls").swiperight(function() {
+    $(this).carousel('prev');
+  });
+  $("#carouselExampleControls").swipeleft(function() {
+    $(this).carousel('next');
+  });
+});
