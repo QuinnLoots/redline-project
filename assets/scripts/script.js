c61ffbbf916e6dcf9d76fe539c641f720a261a18
Quinn Loots <loots.quinn@gmail.com>
2018-12-13 14:33:57 +0100

    test swipeable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {  
-   $("#myCarousel").swiperight(function() {  
-      $("#myCarousel").carousel('prev');  
-    });  
-   $("#myCarousel").swipeleft(function() {  
-      $("#myCarousel").carousel('next');  
-   });  
-});  
+$(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
