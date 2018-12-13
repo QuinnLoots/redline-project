2e4bbd5d9ef162bd2359a1984216c2eecf5a9007
Quinn Loots <loots.quinn@gmail.com>
2018-12-13 15:37:33 +0100

    add photos

--- assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#carouselExampleControls").swiperight(function() {
-    $(this).carousel('prev');
-  });
-  $("#carouselExampleControls").swipeleft(function() {
-    $(this).carousel('next');
-  });
-});
